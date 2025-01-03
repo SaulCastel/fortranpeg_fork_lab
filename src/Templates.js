efe3d7e6dff208c18eea312cb054fc7be028df11
Saul Castellanos <saulcastel018@gmail.com>
2025-01-02 18:46:58 -0600

    Bugfixes en traducción
    - Cambiar el formateo del mensaje de error en fortran para que imprima números mayores a 1 dígito
    - Agregar saltos de linea entre cada traducción de una elección

--- src/Templates.js
+++ src/Templates.js
@@ -108,7 +108,7 @@ module parser
     end function consumeInput
 
     subroutine pegError()
-        print '(A,I1,A)', "Error at ", cursor, ": '"//input(cursor:cursor)//"'"
+        print '(A,I0,A)', "Error at ", cursor, ": '"//input(cursor:cursor)//"'"
 
         call exit(1)
     end subroutine pegError
@@ -162,14 +162,16 @@ export const rule = (data) => `
 export const election = (data) => `
         do i = 0, ${data.exprs.length}
             select case(i)
-            ${data.exprs.map(
-                (expr, i) => `
+            ${data.exprs
+                .map(
+                    (expr, i) => `
             case(${i})
                 cursor = savePoint
                 ${expr}
                 exit
             `
-            )}
+                )
+                .join('\n')}
             case default
                 call pegError()
             end select
